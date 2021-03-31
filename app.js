f3ed50bdb4aaa93881fdf17c56e64c9ce6f65922
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-03-31 12:52:31 +0530

    some set up

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
 var express =require ("express");
 var bodyParser = require("body-parser");
 var app = express();
+
+app.set("view engine", "ejs");
+
 app.get("/",function(req,res)
 {
     var today =new Date();
-    if(today.getDay()==3 || today.getDay()==0 )
+  
+    var day =today.getDay();
+    var dayname="";
+    if(day == 0)
     {
-        res.send("yay! It's a Holiday.")
-    }else{
-        res.send("Uh Oh! It's a working day.")
+        dayname="Sunday";
+        
     }
-});
+    if(day == 1)
+    {
+        dayname="Monday";
+       
+    }
+    if(day == 2)
+    {
+        dayname="Tuesday";
+       
+    }
+    if(day == 3)
+    {
+        dayname="Wednesday";
+       
+    }
+    if(day == 4)
+    {
+        dayname="Thursday";
+       
+    }
+    if(day == 5)
+    {
+       dayName="Friday";
+    };
+    if(day == 6)
+    {
+        dayname="Saturday";
+        
+    };
+    res.render("list",
+        {
+            day:dayname
+            
+        });
+  
+
+}); 
+   
 app.listen(3000,function()
 {
-    console.log("Server is running at port 3000");
-})
+    console.log("Server is running at port 3000.");
+});
